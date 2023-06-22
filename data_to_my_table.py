e45f701da506af476d0cb408435d5c0097bc9283
Behzod Hoshimov <behzod0317@gmail.com>
2023-06-22 15:03:27 +0500

    initial commit

--- data_to_my_table.py
+++ data_to_my_table.py
@@ -1,6 +1,6 @@
 import logging
 
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import datetime ,timedelta
 from fpdf import FPDF
@@ -8,11 +8,11 @@ import unicodedata
 import os
 import psycopg2
 
-# Enable logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-TOKEN = 'YOUR=TOKEN'
+TOKEN = 'YOUR TOKEN'
 
 conn = psycopg2.connect(
     host="localhost",
@@ -29,9 +29,8 @@ dispatcher = updater.dispatcher
 
 def create_attendance_table():
     create_table_query = '''
-    CREATE TABLE IF NOT EXISTS attendance (
+    CREATE TABLE IF NOT EXISTS my_table (
         id SERIAL PRIMARY KEY,
-        user_id INTEGER,
         name VARCHAR(255),
         date DATE,
         came_time TIME,
@@ -46,12 +45,17 @@ create_attendance_table()
 
 class UnicodePDF(FPDF):
     def header(self):
+        self.set_font('Arial', 'B', 16)
+        self.cell(0, 10, 'Davomod hissoboti', align='C', ln=1)
+
        
-        pass
+        
 
     def footer(self):
-        
-        pass
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Copyright 2023 - Quantic-Co', align='C')
+
 
 
 def start(update: Update, context):
@@ -67,34 +71,18 @@ dispatcher.add_handler(start_handler)
 def help(update: Update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="""
-                                Bu bot foydalanuvchilarga botga “+” yoki “-” 
-                                xabarini yuborish orqali o‘z ishtirokini
-                                belgilash imkonini beradi. Bot ishtirokchilar 
-                                haqidagi ma'lumotlarni, jumladan,
-                                foydalanuvchining ismi, sanasi va kelish yoki
-                                ketish vaqtini pdf formatda qaytaradi.""")
+                                *Bu bot foydalanuvchilarga botga “+” yoki “-” xabarini yuborish orqali o‘z ishtirokini belgilash imkonini beradi.Agar kelgan va ketgan vaqtingizni yozmoqchi bo'lsangiz , '+' va '-' dan keyin bir bo'sh joy qoldirib vaqtni kiriting.          Masalan: '+ 9:00'*""",
+                                parse_mode=ParseMode.MARKDOWN)
 
 
-help_handler = CommandHandler('help', help)
+help_handler = CommandHandler('info', help)
 dispatcher.add_handler(help_handler)
+
+
+
 # Dictionary to store attendance information
 attendance_data = {}
 
-
-def collect_attendance(update: Update, context):
-    message_text = update.message.text.strip()
-    user = update.message.from_user
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    current_date = now.strftime("%Y-%m-%d")
-
-    if message_text == '+':
-        if user.id not in attendance_data:
-            attendance_data[user.id] = {
-                'name': user.first_name, 'coming_time': current_time}
-    elif message_text == '-':
-        if user.id in attendance_data:
-            attendance_data[user.id]['leaving_time'] = current_time
 
 
 
@@ -105,7 +93,14 @@ def collect_attendance(update: Update, context):
     current_time = now.strftime("%H:%M")
     current_date = now.strftime("%Y-%m-%d")
 
-    if message_text == '+':
+    if message_text.startswith('+') or message_text.startswith('-'):
+        parts = message_text.split(' ')
+        if len(parts) > 1:
+            time_value = parts[1]
+            if time_value.count(':') == 1:
+                current_time = time_value
+
+    if message_text.startswith('+'):
         if user.id not in attendance_data:
             attendance_data[user.id] = {'name': user.first_name, 'dates': {current_date: {'came': [current_time]}}}
         elif current_date not in attendance_data[user.id]['dates']:
@@ -113,94 +108,17 @@ def collect_attendance(update: Update, context):
         else:
             attendance_data[user.id]['dates'][current_date]['came'].append(current_time)
 
-            
-    elif message_text == '-':
+    elif message_text.startswith('-'):
         if user.id in attendance_data and current_date in attendance_data[user.id]['dates']:
             if 'left' in attendance_data[user.id]['dates'][current_date]:
                 attendance_data[user.id]['dates'][current_date]['left'].append(current_time)
             else:
                 attendance_data[user.id]['dates'][current_date]['left'] = [current_time]
+
+
 message_handler = MessageHandler(
     Filters.text & ~Filters.command, collect_attendance)
 dispatcher.add_handler(message_handler)
-
-# def generate_attendance_table(update: Update, context):
-#     pdf = UnicodePDF()
-#     pdf.add_page()
-#     #font_path = os.path.join(os.path.dirname(__file__), 'dejavu-sans-ttf-2.37', 'DejaVuSans.ttf')
-#     font_path = '/home/behzod/django/fullbot/dejavu-sans-ttf-2.37 (1)/dejavu-sans-ttf-2.37/DejaVuSans.ttf'
-#     pdf.add_font('DejaVuSans', '', font_path, uni=True)
-#     pdf.set_font('DejaVuSans', '', 12)
-
-#     pdf.cell(15, 15, 'ID')
-#     pdf.cell(30, 15, 'User')
-#     pdf.cell(40, 15, 'Date')
-#     pdf.cell(50, 15, 'Came')
-#     pdf.cell(50, 15, 'Left')
-#     pdf.ln()
-
-#     id_counter = 1
-#     for user_id, data in attendance_data.items():
-#         user_name = data.get('name', '')
-#         dates = data.get('dates', {})
-
-#         for date, entry_data in dates.items():
-#             came_times = entry_data.get('came', [])
-#             left_times = entry_data.get('left', [])
-
-#             user_name = str(user_name)
-#             date = str(date)
-
-#         #     pdf.cell(15, 15, str(id_counter))
-#         #     pdf.cell(30, 15, user_name or '')
-#         #     pdf.cell(40, 15, date)
-#         #     pdf.cell(50, 15, ', '.join(came_times))
-#         #     pdf.cell(50, 15, ', '.join(left_times) if left_times else '')
-#         #     pdf.ln()
-#         #     id_counter += 1
-#             # max_len = max(len(came_times), len(left_times))
-
-#             # for i in range(max_len):
-#             #     pdf.cell(15, 15, str(id_counter))
-#             #     pdf.cell(30, 15, user_name or '')
-#             #     pdf.cell(40, 15, date)
-#             #     pdf.cell(50, 15, came_times[i] if i < len(came_times) else '')
-#             #     pdf.cell(50, 15, left_times[i] if i < len(left_times) else '')
-#             #     pdf.ln()
-#             #     id_counter += 1
-       
-
-#     # Calculate the maximum number of rows required for this entry
-#             max_rows = max(len(came_times), len(left_times))
-
-#             for row in range(max_rows):
-#                 pdf.cell(15, 15, str(id_counter))
-#                 pdf.cell(30, 15, user_name if row == 0 else '')  # Display user name only in the first row
-#                 pdf.cell(40, 15, date if row == 0 else '')  # Display date only in the first row
-
-#         # Display the arrival time in the current row
-#                 if row < len(came_times):
-#                     pdf.cell(50, 15, came_times[row])
-#                 else:
-#                     pdf.cell(50, 15, '')  # Leave the cell empty if there are no more arrival times
-
-#         # Display the departure time in the current row
-#                 if row < len(left_times):
-#                     pdf.cell(50, 15, left_times[row])
-#                 else:
-#                     pdf.cell(50, 15, '')  # Leave the cell empty if there are no more departure times
-
-#                 pdf.ln()
-#                 id_counter += 1
-
-
-
-#     pdf_file = 'Attendance.pdf'
-#     pdf.output(pdf_file)
-
-#     with open(pdf_file, 'rb') as file:
-#         context.bot.send_document(
-#             chat_id=update.effective_chat.id, document=file, caption='Davomod hisoboti')
 
 def generate_attendance_table(update: Update, context):
     pdf = UnicodePDF()
@@ -216,7 +134,34 @@ def generate_attendance_table(update: Update, context):
     pdf.cell(50, 15, 'Left')
     pdf.ln()
 
-    id_counter = 1
+
+    conn = psycopg2.connect(
+                        host="localhost",
+                        database="bot",
+                        user="botuser",
+                        password="password1721",
+                        port=5432   
+                                )
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT MAX(id) FROM my_table")
+    last_id = cursor.fetchone()[0] or 0
+
+    cursor.close() 
+    conn.close()
+    id_counter = last_id + 1
+
+    # cursor = conn.cursor()
+    # id_counter = 1
+
+    conn = psycopg2.connect(
+                        host="localhost",
+                        database="bot",
+                        user="botuser",
+                        password="password1721",
+                        port=5432   
+                                )
+    cursor = conn.cursor()
     for user_id, data in attendance_data.items():
         user_name = data.get('name', '')
         dates = data.get('dates', {})
@@ -246,32 +191,44 @@ def generate_attendance_table(update: Update, context):
                     pdf.cell(50, 15, left_times[row])
                 else:
                     pdf.cell(50, 15, '')  # Leave the cell empty if there are no more departure times
-
+                
                 pdf.ln()
+                cursor.execute("INSERT INTO my_table (id, name, date, came_time, left_time) VALUES (%s, %s, %s, %s, %s)",
+                               (id_counter, user_name if row == 0 else '', date if row == 0 else '',
+                                came_times[row] if row < len(came_times) else '',
+                                left_times[row] if row < len(left_times) else ''))
+
                 id_counter += 1
+                
+                conn.commit()
+                
+
+    cursor.close()
+    conn.close()
 
     pdf_file = 'Attendance.pdf'
     pdf.output(pdf_file)
 
     with open(pdf_file, 'rb') as file:
         context.bot.send_document(
-            chat_id=update.effective_chat.id, document=file, caption='Davomad hisoboti')
+            chat_id=update.effective_chat.id, document=file, caption='Davomod hisoboti')
 
 
 def show_menu(update: Update, context):
     menu_options = [
         ['/start', '/menu'],  # List of available commands/options
-        ['/help'],  # to give information
-        ['+'],  # Attend
-        ['-'],  # Leave
+        ['/info'],  # to give information
+        ['+', '-' ],  # came and left work
         ['/hisob'],  # Generate attendance table
-        ['/lastweek'],  # Last week's attendance
-        ['/lastmonth'],  # Last month's attendance
+        ['/lastweek'],  # it sends last week's attendance
+        ['/lastmonth'],  # it sends last month's attendance
     ]
     reply_markup = ReplyKeyboardMarkup(menu_options, resize_keyboard=True)
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Selected option:", reply_markup=reply_markup)
-
+def menu(update: Update, context):
+    show_menu(update, context)
+    
 menu = CommandHandler('menu' , show_menu)
 dispatcher.add_handler(menu)
 
---
+++ example_insert_data.py
@@ -0,0 +1,40 @@
+import psycopg2
+from telebot import TeleBot
+from telebot.types import Message
+import datetime
+bot = TeleBot("5465553669:AAFXOvpFc2UKnCcJUhqUJ1dTzhMoTDSSal8")
+
+
+
+
+DATABASE_URL = "postgres://user:password@host:port/dbname"
+
+
+def connect_to_db():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    return conn
+
+
+def insert_user_data(user_id, join_date, user_info):
+    conn = connect_to_db()
+    cursor = conn.cursor()
+    query = "INSERT INTO users (user_id, join_date, user_info) VALUES (%s, %s, %s)"
+
+    join_date = datetime.date(2023, 6, 22)  
+
+    cursor.execute(query, (user_id, join_date, user_info))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message: Message):
+    user_id = message.from_user.id
+    join_date = message.date
+    user_info = f"{message.from_user.first_name} {message.from_user.last_name}"
+    insert_user_data(user_id, join_date, user_info)
+    bot.reply_to(message, "User information has been stored in the database.")
+
+    # print(message)
+bot.polling()
